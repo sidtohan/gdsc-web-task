410aad2095a84aa0301393bca671e092ed3b37e6
sidtohan <siddhant.tohan@gmail.com>
2022-01-24 20:45:17 +0530

    added profile pic, and also better navbar, still need to add scroll to functionality

--- index.js
+++ index.js
@@ -1,57 +1,88 @@
-const backgroundParticles = (() => {
-  const body = document.body;
-  const getDiv = (index) => {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("bg-div");
+// const backgroundParticles = (() => {
+//   const body = document.body;
+//   const getDiv = (index) => {
+//     let newDiv = document.createElement("div");
+//     newDiv.classList.add("bg-div");
 
-    const height = Math.min(
-      75,
-      Math.max(50, Math.ceil(Math.random() * 10 + 1) * 8)
-    );
-    const width = height;
-    const color = colorPallete[Math.floor(Math.random() * 3)];
+//     const height = Math.min(
+//       75,
+//       Math.max(50, Math.ceil(Math.random() * 10 + 1) * 8)
+//     );
+//     const width = height;
+//     const color = colorPallete[Math.floor(Math.random() * 3)];
 
-    const leftFactor = Math.max(10, Math.ceil(Math.random() * 90));
-    newDiv.setAttribute(
-      "style",
-      `
-    height: ${height}px;
-    width: ${width}px;
-    background-color: ${color};     
-    bottom: -${width}px;
-    left: ${Math.ceil(Math.random() * 100)}%;
-    animation: float-up ${Math.min(
-      30,
-      Math.max(20, Math.floor(Math.random() * 100))
-    )}s linear infinite;
-    opacity: ${Math.min(0.8, Math.max(0.5, Math.random()))};
-    animation-delay: ${index > 25 ? index - 25 : index}s;
-  `
-    );
-    return newDiv;
-  };
-  const colorPallete = ["#4ECDC4", "#FF6B6B", "#FFE66D"];
-  for (let i = 0; i < 30; i++) {
-    body.appendChild(getDiv(i));
-  }
-})();
+//     const leftFactor = Math.max(10, Math.ceil(Math.random() * 90));
+//     newDiv.setAttribute(
+//       "style",
+//       `
+//     height: ${height}px;
+//     width: ${width}px;
+//     background-color: ${color};
+//     bottom: -${width}px;
+//     left: ${Math.ceil(Math.random() * 100)}%;
+//     animation: float-up ${Math.min(
+//       30,
+//       Math.max(20, Math.floor(Math.random() * 100))
+//     )}s linear infinite;
+//     opacity: ${Math.min(0.8, Math.max(0.5, Math.random()))};
+//     animation-delay: ${index > 25 ? index - 25 : index}s;
+//   `
+//     );
+//     return newDiv;
+//   };
+//   const colorPallete = ["#4ECDC4", "#FF6B6B", "#FFE66D"];
+//   for (let i = 0; i < 30; i++) {
+//     body.appendChild(getDiv(i));
+//   }
+// })();
 
 // stores all the refs
 const allRefs = (() => {
-  const navbar = document.querySelector("header");
-  const navBarName = navbar.querySelector(".navigation-name");
-  const navBarButton = navbar.querySelector(".navigation-button");
+  const navBar = document.querySelector("header");
+  const navBarName = navBar.querySelector(".navigation-name");
+  const navBurger = navBar.querySelector(".burger-button");
+  const navBarOptions = navBar.querySelectorAll(".navigation-option");
+
   const home = document.querySelector("#home");
   const skills = document.querySelector("#skills");
   const skillsHeading = skills.querySelector(".section-heading");
   return {
-    navbar,
+    navBar,
     home,
     skills,
     skillsHeading,
     navBarName,
-    navBarButton,
+    navBurger,
+    navBarOptions,
   };
+})();
+
+const navBarLogic = (() => {
+  const options = allRefs.navBarOptions;
+  let current = "home";
+  let currentOptionDiv = options[0];
+
+  const editNav = () => {
+    if (window.scrollY > 3 * allRefs.navBar.offsetHeight) {
+      allRefs.navBar.classList.add("sticky");
+    } else {
+      allRefs.navBar.classList.remove("sticky");
+    }
+  };
+
+  options.forEach((option) =>
+    option.addEventListener("click", () => {
+      if (current === option.classList[1]) {
+        return;
+      }
+      current = option.classList[1];
+      currentOptionDiv.classList.remove("current");
+
+      currentOptionDiv = option;
+      currentOptionDiv.classList.add("current");
+    })
+  );
+  return { editNav };
 })();
 
 const pageBuilder = (() => {
@@ -100,8 +131,7 @@ const pageBuilder = (() => {
             newSpan.textContent = name[length++];
             allRefs.skillsHeading.appendChild(newSpan);
 
-            console.log(newSpan);
-            await waitForMs(300);
+            await waitForMs(200);
           }
         }
       }
@@ -114,7 +144,7 @@ const pageBuilder = (() => {
   );
   const sectionObserver = new IntersectionObserver(sectionObserverCallback, {
     ...options,
-    threshold: 0.1,
+    threshold: 0.5,
   });
 
   const addHomeObserver = (element) => {
@@ -149,9 +179,5 @@ const observeAllSections = () => {
   });
 };
 
-const editNav = () => {
-  if (window.scrollY > 20) {
-  }
-};
 observeAllSections();
-window.addEventListener("scroll", editNav);
+window.addEventListener("scroll", navBarLogic.editNav);
